93dc6687ec9d71d5e678a5562a1dae3343b0f5a3
Marcel Rico <personal.mr4@gmail.com>
2019-12-08 19:52:01 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,44 @@
-# Add your code here
+def run(songs)
+  prompt_user("Welcome to the Jukebox")
+  
+end
+
+def prompt_user(message)
+  puts message
+end
+
+def play(songs)
+  prompt_user("Please enter a song name or number:")
+  user_input = get_user_input
+  
+  if(songs.include?(user_input))
+    puts user_input
+  elsif((user_input.to_i != nil)&&(user_input.to_i >= 1 && user_input.to_i <= songs.length))
+    idx = user_input.to_i - 1
+    puts songs[idx]
+  else
+    puts "Invalid input, please try again"
+  end
+end
+
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def list(songs)
+  songs.length.times do |idx|
+    puts "#{(idx+1)}. #{songs[idx]}"
+  end
+end
+
+def get_user_input
+  gets.chomp
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
